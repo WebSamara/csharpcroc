e0a16a9181ae20f7669c13a4e727c718b59d46da
Дмитрий <Web_samara@mail.ru>
2020-10-15 02:08:02 +0400

    Создание каркаса

---
+++ CShapTelegramBot/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Telegram.Bot;
+
+namespace CShapTelegramBot
+{
+    /// <summary>
+    /// Основной модуль бота
+    /// </summary>
+    public class Bot
+    {
+        /// <summary>
+        /// Клиент Telegram
+        /// </summary>
+        private TelegramBotClient client;
+        /// <summary>
+        /// Конструктор без параметров
+        /// </summary>
+        public Bot() 
+        {
+            client = new TelegramBotClient("1386680338:AAFWdk_x63iwwYmdMxBuRD0ny_CM16e53mI");
+            client.OnMessage += MessageProc;
+        }
+        /// <summary>
+        /// Обработка сообщения
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MessageProc(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            client.SendTextMessageAsync(e.Message.Chat.Id, "Привет!)");
+            Console.WriteLine(e.Message.Text);
+        }
+
+        /// <summary>
+        /// Запуск
+        /// </summary>
+        public void Run() 
+        {
+            //Запуск обработки сообщений
+            client.StartReceiving(); 
+        }
+    }
+}
---
+++ CShapTelegramBot/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Telegram.Bot;
+
+namespace CShapTelegramBot
+{
+    class Program
+    {
+        /// <summary>
+        /// Точка входа
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Bot bot;
+            bot = new Bot();
+            bot.Run();
+            Console.WriteLine("Нажмите ENTER для отключения бота");
+            Console.ReadLine();
+        }
+    }
+}
